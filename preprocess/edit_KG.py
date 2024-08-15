6d23372bef839e0d27e4f74e401d3110b791de6f
Yucheng Shi <38693621+sycny@users.noreply.github.com>
2024-08-15 10:12:23 -0400

    Update edit_KG.py

--- preprocess/edit_KG.py
+++ preprocess/edit_KG.py
@@ -25,10 +25,12 @@ def load_triplets_dict(filename):
     return triplets_dict
 
 def main():
-    filename = '/home/myid/code/ILME/edit/data/wikidata5m_all_triplet.txt'  # Replace this with your file path
-    counterfact_path = '/home/myid/code/ILME/edit/data/MQuAKE-T.json'
-    dict_filename = '/home/myid/code/ILME/edit/data/Wikidata_triplets_dict.pkl'  # Replace this with the desired path for saving the dictionary
-    new_dict_filename = '/home/myid/code/ILME/edit/data/Wikidata_triplets_dict_Edited_T.pkl' 
+    
+    counterfact_path = './data/MQuAKE-T.json'
+    dict_filename = './data/Wikidata_triplets_dict.pkl'  # Replace this with the desired path for saving the dictionary
+    new_dict_filename = './data/Wikidata_triplets_dict_Edited_T.pkl' 
+    
+    
     # Step 1: Load triplets from the file into a dictionary
     triplets_dict = load_triplets_dict(dict_filename)
     lines = load_dataset(counterfact_path)
